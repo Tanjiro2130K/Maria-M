d6ee3ee590eb23d3b263dab275f83bcc1507109d
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-06-29 14:35:31 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,24 +2,24 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['917086726371']
+global.ownername = "TIKU✨🦋"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'SHINOBU🌸' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.stickername = 'SHINOBU🌸'
+global.packname = '☄️Handcrafted for you'
+global.author = 'By SHINOBU💮'
 //console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.themeemoji = '✨'
+global.wm = "TIKU_BOTS"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = 'https://chat.whatsapp.com/DQGWzesuDbz6Qq27pGyWlY'
 
 //custom prefix
 global.prefa = ['.']
